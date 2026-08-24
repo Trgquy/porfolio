67dc8fc888e4a3b2af49d4f2b904d3dd1376dbef
Trgquy <quydttkk3@gmail.com>
2026-08-24 16:22:35 +0700

    first menu

---
+++ test.js
@@ -0,0 +1,11 @@
+const list = document.querySelectorAll(".list");
+
+list.forEach((item) => {
+  item.addEventListener("click", function () {
+    list.forEach((li) => {
+      li.classList.remove("active");
+    });
+
+    this.classList.add("active");
+  });
+});
